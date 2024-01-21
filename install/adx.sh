829c02ca06ed09bbb81fbe67c3854bd3d325e8b5
qq2q <149266511+qq2q@users.noreply.github.com>
2024-01-21 03:06:47 +0300

    Create adx.sh

---
+++ install/adx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if Python 3 is installed
+if pkg list-installed | grep -q "python"; then
+    echo "Python 3 is installed."
+    reset 
+    curl -s "https://raw.githubusercontent.com/qq2q/3/main/bot-ios.py" > bot.py 
+    python3 bot.py
+else
+    echo "Python 3 is not installed."
+    termux-change-repo
+    pkg update
+    pkg install python3
+    pip3 install telethon requests python-telegram-bot
+    reset 
+    curl -s "https://raw.githubusercontent.com/qq2q/3/main/bot-ios.py" > bot.py 
+    python3 bot.py
+fi
